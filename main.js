e093deedda5d50351aa15762371bef9fdaae5ccd
Oussama abdallah <oussama.abdellah@gmail.com>
2017-03-15 18:06:18 +0400

    Making a change on the timeOut value

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var promise = new Promise((resolve, reject) => {
 
     setTimeout(function () {
         resolve("Oussaki"); //Yay! Everything went well!
-    }, 450);
+    }, 800);
 
 });
 
 promise.then((value) => {
-    value = "Hello " + value
+    value = "Hello {" + value+ "}"
     return value
 })
 
